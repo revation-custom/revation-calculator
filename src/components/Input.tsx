e79206195ec3303f25196cf84bf5ca73bba320a0
jkim <jkim@blast-team.com>
2024-10-21 17:53:17 +0900

    feat: textField error field

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,6 @@
 import { clsx } from "clsx";
 import { Field, FieldError } from "react-hook-form";
+import { Typography } from "./Typography.tsx";
 
 interface InputProps {
   field: Field;
@@ -15,15 +16,22 @@ export const Input = ({
   disabled = false,
 }: InputProps) => {
   return (
-    <input
-      {...field}
-      placeholder={placeholder ?? ""}
-      disabled={disabled}
-      className={clsx(
-        "font-pretendard text-xl border p-[10px] font-normal text-md placeholder-gray-500 outline-none hover:text-font hover:ring-[0.5px] hover:ring-solid border-solid",
-        error?.message ? "border-red-500" : "border-black",
-        disabled ? "bg-bg-200 opacity-60 placeholder-gray-500" : "",
+    <div className="font-pretendard flex flex-col gap-1">
+      <input
+        {...field}
+        placeholder={placeholder ?? ""}
+        disabled={disabled}
+        className={clsx(
+          "duration-200 text-xl border p-[10px] font-normal text-md placeholder-gray-500 outline-none hover:text-font hover:ring-[0.5px] hover:ring-solid border-solid",
+          error?.message ? "border-bg-500" : "border-black",
+          disabled ? "bg-bg-200 opacity-60 placeholder-gray-500" : "",
+        )}
+      />
+      {error?.message && (
+        <Typography variant="errorText" color="text-bg-500">
+          {error?.message}
+        </Typography>
       )}
-    />
+    </div>
   );
 };
