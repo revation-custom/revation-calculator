0de5ec3f5d10e9e23613a50374598ccf4888b5ab
jkim <jkim@blast-team.com>
2024-10-31 16:32:19 +0900

    fix: qa

--- src/components/RevationResultBox.tsx
+++ src/components/RevationResultBox.tsx
@@ -1,16 +1,21 @@
 import { UNIT } from '../constants/common';
+import { Plastic } from '../types/form';
 import { formatNumber } from '../utils/formatNumber';
 import { Typography } from './Typography';
 
 interface RevationResultBoxProps {
+  label: Plastic;
   resultData: number;
 }
 
-export const RevationResultBox = ({ resultData }: RevationResultBoxProps) => {
+export const RevationResultBox = ({
+  label,
+  resultData,
+}: RevationResultBoxProps) => {
   return (
-    <div className="result-box flex h-[210px] w-full flex-col items-center justify-center gap-3 py-[125px] xs:h-[474px] sm:h-[350px] md:h-[382px] md:w-full">
+    <div className="result-box flex aspect-[4/3] flex-1 flex-col items-center justify-center gap-3 py-[125px] sm:aspect-auto md:aspect-[4/3]">
       <Typography className="body-lg" color="text-bg-100">
-        소재 01
+        {label}
       </Typography>
       <div className="flex flex-col items-center">
         <Typography className="heading-sm leading-[45.6px]" color="text-bg-100">
