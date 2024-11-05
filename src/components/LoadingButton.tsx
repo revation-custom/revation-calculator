317d07773f8677c72be07ea13e288fc3b36aa322
jkim <jkim@blast-team.com>
2024-11-05 16:25:52 +0900

    feat: duplication popup

--- src/components/LoadingButton.tsx
+++ src/components/LoadingButton.tsx
@@ -1,19 +1,30 @@
+import clsx from 'clsx';
 import { IcSpin } from '../assets/icons/IcSpin.tsx';
 
 interface CustomButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   loading: boolean;
+  variant: keyof typeof buttonStyle;
 }
+
+const buttonStyle = {
+  lg: 'h-[50px] w-[128px] sm:h-[68px] sm:w-[232px]',
+  sm: 'h-[46px] w-[76px]',
+};
 
 export const LoadingButton = ({
   children,
   loading,
   disabled = false,
+  variant,
   ...props
 }: CustomButtonProps) => {
   return (
     <button
-      className="transition-2 group flex h-[50px] w-[128px] select-none items-center justify-center rounded-[34px] bg-primary-600 duration-300 hover:bg-primary-500 disabled:bg-primary-100 sm:h-[68px] sm:w-[232px]"
+      className={clsx(
+        buttonStyle[variant],
+        'transition-2 group flex select-none items-center justify-center rounded-[34px] bg-primary-600 duration-300 hover:bg-primary-500 disabled:bg-primary-100',
+      )}
       disabled={disabled}
       {...props}
     >
